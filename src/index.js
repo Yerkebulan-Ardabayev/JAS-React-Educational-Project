6928e2f97d96008deda36bf0575b69c58d130aad
Yerkebulan Ardabayev <yerkebulan.ardabayev@gmail.com>
2022-05-04 19:15:50 +0600

    delete some files and add search, modify pagination, map

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from 'react';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store";
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
       <Provider store={store}>
           <BrowserRouter>
               <App />
           </BrowserRouter>
       </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
 );
--- src/pages/CinemaPages.js
+++ src/pages/CinemaPages.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { CinemaPage } from "./CinemaPage";
 import { Pagination } from '@mui/material';
@@ -7,10 +7,13 @@ import { scrollToTop } from "../utils/scrollToTop";
 export function CinemaPages() {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [totalPages, setTotalPages] = useState(4);
+  
 
-  const fetchMovies = (page = 1) => {
+  const fetchMovies = (page = 1, search = "") => {
     fetch(
-      `https://www.breakingbadapi.com/api/characters?limit=20&offset=${(page - 1) * 20}`
+      `https://www.breakingbadapi.com/api/characters/?name=${search}&limit=20&offset=${(page - 1) * 20}`
     )
       .then((res) => res.json())
       .then((res) => {
@@ -19,13 +22,44 @@ export function CinemaPages() {
       });
   };
 
-  useEffect(() => {
+  function handelSubmit(e) { 
+    e.preventDefault();
+    fetchMovies(1, search);
+    if (movies.length <= 20 && search !== "") {
+      setTotalPages(1);
+    }
+    else {
+      setTotalPages(4)
+    };
+  }
 
+  useEffect(() => {
     fetchMovies(page);
   }, [page]);
 
   return (
-    <div>
+    <div style={{
+      padding: "20px",
+    }}>
+      <form onSubmit={ handelSubmit}
+        style={{
+        display: "flex",
+        justifyContent: "flex-end",
+      }}>
+        <TextField onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+          id="outlined-search" label="Search field" type="search" value={search}/>
+        <Button style={{
+          backgroundColor: "blue",
+          color: "white",
+        }}
+          type="submit"
+          variant="outlined"
+        >
+          Поиск по имени
+        </Button>
+      </form>
       <h1
         style={{
           margin: 0,
@@ -38,26 +72,36 @@ export function CinemaPages() {
       >
         The Breaking Bad
       </h1>
-      <Grid container spacing={2}>
-        {movies.map((movie) => (
-          <Grid item lg={12 / 5}>
-            <CinemaPage movie={movie} />
-          </Grid>
-        ))}
+      <Grid  container spacing={2}>
+        {!movies.length ? 
+          <h1 style={
+            {               
+              fontSize: "4rem",
+              color: "rgb(32, 35, 41)",
+              margin: "0 auto",
+            }
+          }>Нет данных</h1> :
+          movies.map((movie) => (
+            <Grid item lg={12 / 5} key={movie.char_id}>
+              <CinemaPage movie={movie} />
+            </Grid>
+          ))}
       </Grid>
-
       <div style={{ display: "flex", margin: "20px auto" }}>
-        <Pagination
-          style={{ margin: "0 auto" }}
-          count={4}
-          page={page}
-          onChange={(event, page) => {
-          setPage(page)
-          }}
-          variant="outlined"
-          shape="rounded"
-          color="secondary"
-        />
+        {!movies.length ? 
+          "" : 
+          <Pagination
+            style={{ margin: "0 auto" }}
+            count={totalPages}
+            page={page}
+            onChange={(event, page) => {
+              setPage(page)
+            }}
+            variant="outlined"
+            shape="rounded"
+            color="secondary"
+          />
+        }
       </div>
     </div>
   );
